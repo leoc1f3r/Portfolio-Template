c82aafe06cc617aca4975d49cac1cdd7afdc935d
Lionel Dsilva <lionelcd94@gmail.com>
2016-07-15 14:23:13 +0530

    Added all files

---
+++ assets/js/custom.js
@@ -0,0 +1,35 @@
+jQuery(function($) {
+    /*==========================================
+    CUSTOM SCRIPTS
+    =====================================================*/
+
+    // CUSTOM LINKS SCROLLING FUNCTION 
+
+    $('a[href*=#]').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+       && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target
+            || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                return false;
+            }
+        }
+    });
+
+    /*==========================================
+   SCROLL REVEL SCRIPTS
+   =====================================================*/
+
+    
+       window.scrollReveal = new scrollReveal(); 
+    
+
+    /*==========================================
+    WRITE  YOUR  SCRIPTS BELOW
+    =====================================================*/
+
+});
